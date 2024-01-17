ea86f8c3c073ab8393ae0a38659173105b5f5626
sachko65 <levchencow@gmail.com>
2024-01-17 14:18:32 +0200

    Initial Bird

---
+++ src/Bird.java
@@ -0,0 +1,17 @@
+public class Bird {
+    private String name;
+    public Bird(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String fly(){
+        return name + " літає";
+    }
+}
